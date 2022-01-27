f7bad97462a41c740e661e1297f5eb2cd3b53a03
Meghaapr6 <meghaapr6@gmail.com>
2022-01-27 16:53:26 +0530

    first commit

---
+++ ex.py
@@ -0,0 +1,11 @@
+userInpt=input('enter 1 or 2') 
+if userInpt=='1': 
+    print("hello")
+elif userInpt=='2':
+    print("welcome")
+elif userInpt=='3':
+    print("welcome to my python project")
+
+
+else:
+    print("invalid")
